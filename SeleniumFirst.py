f8865469541b736840999b72f85baf3c17326ef8
sathishrpt24 <sathishrpt24@gmail.com>
2019-01-04 22:30:47 +0530

    - Selenium_Python
    - First Programm

---
+++ SeleniumFirst.py
@@ -0,0 +1,9 @@
+from selenium import webdriver;
+from selenium.webdriver.common.keys import Keys
+
+url = "https://www.facebook.com";
+driver = webdriver.Chrome(executable_path="C:\\Users\\acer\\Desktop\\Drivers\\chromedriver.exe");
+driver.get(url);
+print(driver.title);
+
+driver.close();
